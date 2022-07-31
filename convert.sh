3a6e9446bd2fb2af351443b23b649f8d555e1245
Northword <northword@outlook.com>
2022-07-31 17:22:45 +0800

    fix for premature deletion of tex files

--- convert.sh
+++ convert.sh
@@ -8,8 +8,9 @@ cp ./tex/image/* ./docs
 cp ./tex/*.tex ./docs
 if (  type pandoc  ); then
     pandoc -s ./docs/Ezotero_intro.tex -o ./docs/Ezotero.md
+    rm ./docs/*.tex
 else
     echo 'nope, no pandoc installed.'
     echo 'This message can be ignored if it is run in GitHub actions, and the docker will run pandoc later.'
 fi
-rm ./docs/*.tex
+
