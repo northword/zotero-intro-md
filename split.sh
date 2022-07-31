c651f0bd45c3efee6f38fc75eea5e9b9f64e3cbc
Northword <northword@outlook.com>
2022-07-31 20:43:02 +0800

    improve split file name

--- split.sh
+++ split.sh
@@ -1,7 +1,10 @@
 cd ./docs
+
+# awk '{ gsub(/{.*}/,""); # print $0 }' Ezotero.md
+
 awk '($1=="#"){
         close(out)
-        out=$2".md" 
+        out="chapter"(c++)".md" 
       } 
       out!=""{
         print > out
@@ -11,7 +14,8 @@ rm Ezotero.md
 if [ ! -d "faqs" ]; then
   mkdir faqs
 fi
-mv 高级应用及疑难解答.md faqs/
+mv chapter5.md faqs/
 cd faqs
-awk '($1=="##"){ close(out); out=$2".md" } out!=""{print > out}' 高级应用及疑难解答.md
-rm 高级应用及疑难解答.md
+# awk '($1=="##"){ close(out); out=$2".md" } out!=""{print > out}' 高级应用及疑难解答.md
+awk '($1=="##"){ close(out); out=(c++)".md" } out!=""{print > out}' chapter5.md
+rm chapter5.md
