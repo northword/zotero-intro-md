453a11db1f1ae8ab417bbfb2a389fcedb2a5695b
Northword <northword@outlook.com>
2022-07-31 18:43:02 +0800

    set pandoc markdown-headings to atx

--- convert.sh
+++ convert.sh
@@ -11,11 +11,11 @@ if (  type pandoc  ); then
     rm ./docs/*.tex
 else
     echo 'nope, no pandoc installed.'
-    # echo 'This message can be ignored if it is run in GitHub actions, and the docker will run pandoc later.'
+    echo 'This message can be ignored if it is run in GitHub actions, and the docker will run pandoc later.'
     # wget https://github.com/jgm/pandoc/releases/download/2.18/pandoc-2.18-linux-amd64.tar.gz
     # tar
-    conda install pandoc
-    pandoc -s ./docs/Ezotero_intro.tex -o ./docs/Ezotero.md
-    rm ./docs/*.tex
+    # conda install pandoc
+    # pandoc -s ./docs/Ezotero_intro.tex -o ./docs/Ezotero.md
+    # rm ./docs/*.tex
 fi
 
